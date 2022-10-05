ab7c0f130b85d0c9ca5d3cd851ea7612fb5fa610
Valentin Moise <moisevalentin98@gmail.com>
2022-10-05 21:22:49 +0300

    Update and rename 66. Plus One.py to plusOne.py

--- Python/Leetcode/plusOne.py
+++ Python/Leetcode/plusOne.py
@@ -1,4 +1,10 @@
-#https://leetcode.com/problems/plus-one/
+"""
+Leetcode Problem #66
+Link: https://leetcode.com/problems/plus-one/
+
+Name: Valentin Moise
+Github: MoiseValentin (https://github.com/MoiseValentin)
+"""
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         for index in range(len(digits)-1, -1, -1):
